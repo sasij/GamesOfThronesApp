868e2de3b8c4fd686efdd6075f69bf180f80a858
Juanjo <sanchez.silos@gmail.com>
2015-12-08 21:12:44 +0100

    first commit

---
+++ app/src/main/java/me/juanjo/gamesofthrones/AppApplication.java
@@ -0,0 +1,11 @@
+package me.juanjo.gamesofthrones;
+
+import android.app.Application;
+
+/**
+ * Created by juanjo on 8/12/15.
+ */
+public class AppApplication extends Application {
+    private Application application;
+
+}
---
+++ app/src/main/java/me/juanjo/gamesofthrones/injects/modules/ApplicationModule.java
@@ -0,0 +1,33 @@
+package me.juanjo.gamesofthrones.injects.modules;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import javax.inject.Singleton;
+
+import dagger.Provides;
+
+/**
+ * Created by juanjo on 8/12/15.
+ */
+public class ApplicationModule {
+
+    private Application application;
+
+    public ApplicationModule(Application app) {
+        application = app;
+    }
+
+    @Provides
+    @Singleton
+    Application getContext() {
+        return application;
+    }
+
+    @Provides
+    @Singleton
+    SharedPreferences getSharedPreferences() {
+        return PreferenceManager.getDefaultSharedPreferences(application);
+    }
+}
